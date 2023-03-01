6533355526e7ca5f9228306791a840c5d5315d60
Lucas Campos Olv <118633611+lucascamposolv@users.noreply.github.com>
2023-02-28 21:44:05 -0300

    Add files via upload

---
+++ Exercicios/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDllCalculadora
+{
+    public class Calculadora
+    {
+        #region [propriedades publicas -> encapsulamento]
+        private double N1 { get; set; }//propriedade
+        #endregion
+
+        #region [atributos da classe -> de regra devem ser privados]
+        private double N2; //atributo
+        #endregion  
+
+        #region [Getters e Setters -> encapsulamento ]
+        public void SetN2(double n2)
+        {
+            this.N2 = n2;
+        }
+        public double GetN2()
+        {
+            return this.N2;
+        }
+        #endregion
+
+        #region [METODOS PUBLICOS DA CLASSE]
+        public double Somar(double n1, double n2)
+        {
+            double Adicao = 0;
+            this.N1 = n1; this.N2 = n2;
+            Adicao = this.N1 + this.N2;
+            return Adicao;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoDllCalculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTesteCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Calculadora c1 = new   Calculadora();
+            double result = c1.Somar(3.5, 4.5);
+            Console.WriteLine("soma={0}", result);
+
+           Console.ReadLine();
+        }
+    }
+}
